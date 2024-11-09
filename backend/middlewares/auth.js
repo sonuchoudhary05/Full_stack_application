d44f097db852e0b54eb7fca38a2f6b614f04e6dc
sonukumar-trustt <160998790+sonukumar-trustt@users.noreply.github.com>
2024-11-10 01:04:12 +0530

    controller, routes, models created

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate = (req, res, next) => {
+  const token = req.header("Authorization").replace("Bearer ", "");
+  if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
+
+module.exports = authenticate;
